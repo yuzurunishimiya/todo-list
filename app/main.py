1f5129634e04dc04c06ba7e7faca7627286d4bcb
Sony Alamsyah Megadiana <sonyalamsyahmegadiana92@gmail.com>
2023-10-06 20:29:28 +0700

    add priorities routers

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from flask import jsonify
 
 from .routes.tasks import todo
 from .routes.status import statuses
+from .routes.priorities import bp_priorities
 
 
 app = Flask(__name__)
 app.register_blueprint(todo)
 app.register_blueprint(statuses)
+app.register_blueprint(bp_priorities)
 
 
 @app.errorhandler(404)
---
+++ app/routes/priorities.py
@@ -0,0 +1,34 @@
+from flask import Blueprint
+from flask import abort, jsonify
+
+# pylint: disable=E0611
+from app.database.db import conn
+
+
+bp_priorities = Blueprint("bp_priorities", __name__, url_prefix="/priorities")
+
+
+@bp_priorities.route("", methods=["GET"])
+def get_priorities():
+    cur = conn.cursor()
+    cur.execute("SELECT id, priority FROM priorities")
+    data = cur.fetchall()
+
+    column_name = ["id", "priority"]
+    lod_result = [dict(zip(column_name, row)) for row in data]
+
+    return jsonify(lod_result)
+
+
+@bp_priorities.route("/<int:priority_id>", methods=["GET"])
+def get_priority(priority_id):
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, priority FROM priorities WHERE id={}".format(priority_id)
+    )
+    datum = cur.fetchone()
+
+    if datum is None:
+        abort(404, description="Resource not found")
+
+    return jsonify(id=datum[0], priority=datum[1])
