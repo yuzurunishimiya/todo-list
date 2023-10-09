d8cd20913c60001269b22001114585b1f7a8d738
Sony Alamsyah Megadiana <sonyalamsyahmegadiana92@gmail.com>
2023-10-09 17:09:36 +0700

    add task api to app

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import jsonify
 
-from .routes.tasks import todo
+from .routes.tasks import bp_todo
 from .routes.priorities import bp_priorities
 from .routes.statuses import bp_statuses
 
 
 app = Flask(__name__)
-app.register_blueprint(todo)
+app.register_blueprint(bp_todo)
 app.register_blueprint(bp_statuses)
 app.register_blueprint(bp_priorities)
 
--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,26 +1,63 @@
 from flask import Blueprint, request
-from flask import jsonify
-
+from flask import abort, jsonify
 from marshmallow import ValidationError
-from ..schemas.requests.tasks import TaskAdd
+
+from app.database.db import conn
+from app.schemas.requests.tasks import TaskAdd
 
 
-todo = Blueprint('todo', __name__, url_prefix="/tasks")
+bp_todo = Blueprint("bp_todo", __name__, url_prefix="/tasks")
 
 
-@todo.route('', methods=['POST', 'GET'])
+@bp_todo.route("", methods=["POST", "GET"])
 def add_task():
-    if request.method == 'POST':
+    if request.method == "POST":
         body = request.json
         schema = TaskAdd()
 
         try:
             # validate request json
-            result = schema.load(body)
+            body = schema.load(body)
+            # data = f'({body.get("task")}, {body.get("status_id")}, {body.get("priority_id")}, {body.get("start_date_time")}, {body.get("end_date_time")})'
+            cur = conn.cursor()
+            cur.execute(
+                f"INSERT INTO tasks (task, status_id, priority_id, start_date_time, end_date_time) VALUES(%s, %s, %s, %s, %s) RETURNING id",
+                (
+                    body.get("task"),
+                    body.get("status_id"),
+                    body.get("priority_id"),
+                    body.get("start_date_time"),
+                    body.get("end_date_time"),
+                ),
+            )
+            id_of_new_row = cur.fetchone()[0]
+            if id_of_new_row is None:
+                abort(400, description="gagal insert data")
+
+            conn.commit()
+
+            return (
+                jsonify(status="success", message="insert data berhasil"),
+                200,
+            )
+
         except ValidationError as error:
             return jsonify(error.messages), 400
 
-        return "ok"
-
     else:
-        return "todos"
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT id, task, status_id, priority_id, start_date_time, end_date_time FROM tasks"
+        )
+        data = cur.fetchall()
+        column_name = [
+            "id",
+            "task",
+            "status_id",
+            "priority_id",
+            "start_date_time",
+            "end_date_time",
+        ]
+        lod_result = [dict(zip(column_name, row)) for row in data]
+
+        return jsonify(lod_result)
