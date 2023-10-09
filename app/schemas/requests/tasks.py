75c4ae29758f5e5da24dd810a3ee5fe7822b3534
Sony Alamsyah Megadiana <sonyalamsyahmegadiana92@gmail.com>
2023-10-09 17:08:44 +0700

    add missing field at TaskAdd Scheme

--- app/schemas/requests/tasks.py
+++ app/schemas/requests/tasks.py
@@ -5,6 +5,7 @@ class TaskAdd(Schema):
     """request JSON of add task"""
 
     task = fields.String(required=True)
+    status_id = fields.Int(strict=True)
     priority_id = fields.Int(strict=False)
     start_date_time = fields.Str(required=False, default=None)
     end_date_time = fields.Str(required=False, default=None)
